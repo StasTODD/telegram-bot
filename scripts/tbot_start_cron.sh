f0144f56a01e3c4e737875c9a6064eb548bb8574
StasTODD <todd.stas@gmail.com>
2020-11-15 19:07:09 +0200

    Upd: refactor start script

--- scripts/tbot_start_cron.sh
+++ scripts/tbot_start_cron.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 tbot_script_file=main.py
-tbot_script_path=/home/$USER/projects/telegram-bot/$tbot_script_file
-tbot_script_pid=$(ps -axx | grep telegram | grep main.py | awk '{print $1}')
+tbot_script_path=/home/stastodd/projects/telegram-bot/
+tbot_script_pid=$(ps -axx | grep telegram-bot | grep main.py | awk '{print $1}')
 
 if [ $tbot_script_pid ]
   then
     echo "tbot script is running, it have pid: $tbot_script_pid"
   else
-    $tbot_script_path
+    cd $tbot_script_path ; ./$tbot_script_file
 fi
