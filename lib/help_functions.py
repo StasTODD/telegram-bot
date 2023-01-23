91ff718570ed01f66be63703f02352b06e923985
Stas Goncharenko <todd.stas@gmail.com>
2023-01-24 00:35:23 +0200

    Add: PID functionality

--- lib/help_functions.py
+++ lib/help_functions.py
@@ -1,7 +1,10 @@
-import yaml
-import aiohttp
-from typing import List, Dict, Union, Any
+import os
+import signal
 from pathlib import Path
+from typing import Dict, Union
+import asyncio
+import aiohttp
+import yaml
 
 
 def get_data_from_yaml(filename: str) -> dict:
@@ -42,7 +45,56 @@ def create_dir(dir_path: str):
     Path(dir_path).mkdir(parents=True, exist_ok=True)
 
 
+async def find_pid(attribute: str) -> Union[int, str, bool]:
+    """
+    Find PID using keyword for searching.
+    :param attribute: "rpi_pirsensor_camera_telegram"
+    :return: int | str | bool
+        int - number of PID
+        str - error text message
+        bool - (False) PID not found
+    """
+    # https://queirozf.com/entries/python-3-subprocess-examples#wait-for-command-to-terminate-asynchronously
+    command = f"ps ax | grep {attribute} | grep -v grep | awk '{{print $1}}'"
+    proc = await asyncio.create_subprocess_shell(command,
+                                                 stdout=asyncio.subprocess.PIPE,
+                                                 stderr=asyncio.subprocess.PIPE)
+    stdout, stderr = await proc.communicate()
+    error_msg = stderr.decode()  # ''
+    pid = stdout.decode()  # '1234\n'
+    result = False
+    if pid:
+        try:
+            result = int(pid)
+        except ValueError as e:
+            result = e
+    if error_msg:
+        return error_msg
+    if not error_msg and not pid:
+        return False
+    return result
+
+
+async def kill_process(pid: int) -> Union[str, bool]:
+    """
+    Kill PID with SIGKILL option (-9)
+    :param pid: int | 3126
+    :return: True | 'error text'
+    """
+    try:
+        os.kill(int(pid), signal.SIGKILL)
+        return True
+    except ProcessLookupError as e:
+        return e
+    except PermissionError as e:
+        return e
+    except Exception as e:
+        return e
+
+
 __all__ = ["get_data_from_yaml",
            "admin_check",
            "get_json_from_web",
-           "create_dir"]
+           "create_dir",
+           "find_pid",
+           "kill_process"]
